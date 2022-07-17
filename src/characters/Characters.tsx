d0b3cd106bb4c1899802016c09a5271d5411c121
filschod <2967194+filschod@users.noreply.github.com>
2022-07-18 01:02:16 +0200

    Add status filters

--- src/characters/Characters.tsx
+++ src/characters/Characters.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
 
 const GET_CHARACTERS = gql`
-    query getCharacters($page: Int, $searchWord: String) {
-        characters(page: $page, filter: { name: $searchWord }) {
+    query getCharacters($page: Int, $searchWord: String, $selectedStatus: String) {
+        characters(page: $page, filter: { name: $searchWord, status: $selectedStatus }) {
             results {
                 name
             }
@@ -17,13 +17,38 @@ interface CharacterModel {
 const Characters = (): JSX.Element => {
     const [page] = useState(1);
     const [searchWord, setSearchWord] = useState('');
+    const [selectedStatus, setSelectedStatus] = useState('');
     const { data, loading } = useQuery(GET_CHARACTERS, {
-        variables: { page, searchWord },
+        variables: { page, searchWord, selectedStatus },
     });
 
     return (
         <div>
             <input value={searchWord} onChange={(e) => setSearchWord(e.target.value)} />
+            <input
+                type='checkbox'
+                checked={selectedStatus === ''}
+                onChange={(e) => e.target.checked && setSelectedStatus('')}
+            />{' '}
+            Any
+            <input
+                type='checkbox'
+                checked={selectedStatus === 'Alive'}
+                onChange={(e) => e.target.checked && setSelectedStatus('Alive')}
+            />{' '}
+            Alive
+            <input
+                type='checkbox'
+                checked={selectedStatus === 'Dead'}
+                onChange={(e) => e.target.checked && setSelectedStatus('Dead')}
+            />{' '}
+            Dead
+            <input
+                type='checkbox'
+                checked={selectedStatus === 'Unknown'}
+                onChange={(e) => e.target.checked && setSelectedStatus('Unknown')}
+            />{' '}
+            Unknown
             {loading ? <p>loading...</p> : data.characters.results.map(({ name }: CharacterModel) => <div>{name}</div>)}
         </div>
     );
