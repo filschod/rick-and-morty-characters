890f1e0dd414cb2818ea0b4f0f61af0efa3541d6
filschod <2967194+filschod@users.noreply.github.com>
2022-07-18 01:59:21 +0200

    Add infinite scroll pagination

--- src/characters/Characters.tsx
+++ src/characters/Characters.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { UIEvent, useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
 
 const GET_CHARACTERS = gql`
@@ -15,15 +15,42 @@ interface CharacterModel {
 }
 
 const Characters = (): JSX.Element => {
-    const [page] = useState(1);
+    const [page, setPage] = useState(1);
     const [searchWord, setSearchWord] = useState('');
     const [selectedStatus, setSelectedStatus] = useState('');
-    const { data, loading } = useQuery(GET_CHARACTERS, {
-        variables: { page, searchWord, selectedStatus },
+    const { data, fetchMore } = useQuery(GET_CHARACTERS, {
+        variables: { page: 1, searchWord, selectedStatus },
     });
 
+    const loadMoreCharacters = (): void => {
+        fetchMore({
+            variables: {
+                page: page + 1,
+                searchWord,
+                selectedStatus,
+            },
+            updateQuery: (prevState, { fetchMoreResult }) => {
+                if (!fetchMoreResult) return prevState;
+                const newCharacters = {
+                    characters: {
+                        __typename: 'Characters',
+                        results: [...prevState.characters.results, ...fetchMoreResult.characters.results],
+                    },
+                };
+                return newCharacters;
+            },
+        });
+        setPage((prevState) => prevState + 1);
+    };
+
+    const scrollHandler = ({ currentTarget }: UIEvent, loadMore: { (): void; (): void }): void => {
+        if (currentTarget.scrollTop + currentTarget.clientHeight >= currentTarget.scrollHeight) {
+            loadMore();
+        }
+    };
+
     return (
-        <div>
+        <div style={{ maxHeight: '100vh', overflowY: 'auto' }} onScroll={(e) => scrollHandler(e, loadMoreCharacters)}>
             <input value={searchWord} onChange={(e) => setSearchWord(e.target.value)} />
             <input
                 type='checkbox'
@@ -49,7 +76,10 @@ const Characters = (): JSX.Element => {
                 onChange={(e) => e.target.checked && setSelectedStatus('Unknown')}
             />{' '}
             Unknown
-            {loading ? <p>loading...</p> : data.characters.results.map(({ name }: CharacterModel) => <div>{name}</div>)}
+            {/* {loading && <p style={{ position: 'fixed', top: 100, left: 100 }}>loading...</p>} */}
+            {data?.characters?.results?.map(({ name }: CharacterModel) => (
+                <div style={{ height: '200px' }}>{name}</div>
+            ))}
         </div>
     );
 };
